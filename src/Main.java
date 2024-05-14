876bf621aefa598bf9fafd231c3801bfc9a38339
João Gabriel <jgabriiell33@gmail.com>
2024-05-14 09:10:40 -0300

    Cirando classes Curso e Mentoria

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Curso curso = new Curso();
+        Mentoria mentoria = new Mentoria();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        curso.setTitulo("Java");
+        curso.setDescricao("Aprendendo Java");
+        curso.setCargaHoraria(80);
+        System.out.println(curso);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        mentoria.setTitulo("Importancia da lógica");
+        mentoria.setDescricao("Entendendo lógica de programação");
+        mentoria.setData(LocalDate.now());
+        System.out.println(mentoria);
     }
 }
